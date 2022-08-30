11cba1f1eec4fa89aa94996c658ac446d800088d
Diksha <dikshapatidar0149@gmail.com>
2022-08-30 11:08:42 +0530

    resolved icon error by adding new link of font awesome

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const mobile_nav = document.querySelector(".mobile-navbar-btn");
 const nav_header = document.querySelector(".header");
+const navbar = document.querySelector("#navbar");
 
-const toggleNavbar = () => {
-  // alert("Plz Subscribe ");
+const toggleNavbar = (e) => {
   nav_header.classList.toggle("active");
 };
 
-mobile_nav.addEventListener("click", () => toggleNavbar());
+mobile_nav.addEventListener(
+  "click",
+  function(e){
+    toggleNavbar(e)
+  }
+  //  (e) => toggleNavbar(e)
+);
+navbar.addEventListener(
+  "click",
+  function(e){
+  //  console.log(e);
+  }
+)
